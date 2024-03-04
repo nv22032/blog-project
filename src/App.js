cba6e2e0eaf43de6a4be3fa166bf1581d20f39d1
nv22032 <150386492+nv22032@users.noreply.github.com>
2024-03-04 18:55:33 +0000

    4 - Responsive Header

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import HomePage from "./pages/Homepage";
+
+function App() {
+  return (
+    <div className="App font-opensans">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
