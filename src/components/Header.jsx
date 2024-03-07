251589a7170fea0352a084010c47fbe573e64e1a
nv22032 <150386492+nv22032@users.noreply.github.com>
2024-03-07 05:58:16 +0000

    5 - Hero Section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,6 +13,14 @@ const navItemsInfo = [
 ];
 
 const NavItem = ({ item }) => {
+  const [dropdown, setDropdown] = useState(false);
+
+  const toggleDropdownHandler = () => {
+    setDropdown((curState) => {
+      return !curState;
+    });
+  };
+
   return (
     <li className="relative group">
       {item.type === "link" ? (
@@ -20,19 +28,26 @@ const NavItem = ({ item }) => {
           <a href="/" className="px-4 py-2">
             {item.name}
           </a>
-          <span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
+          <span className="cursor-pointer text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">
             /
           </span>
         </>
-    ) : (
-        <>
-          <a href="/" className="px-4 py-2 flex gap-x-1 items-center">
+      ) : (
+        <div className="flex flex-col items-center">
+          <button
+            className="px-4 py-2 flex gap-x-1 items-center"
+            onClick={toggleDropdownHandler}
+          >
             <span>{item.name}</span>
             <MdKeyboardArrowDown />
-          </a>
-          <div className="hidden transition-all duration-500 pt-4 absolute bottom-0 right-0 transform translate-y-full group-hover:block w-max">
-            <ul className="flex flex-col shadow-lg rounded-lg overflow-hidden">
-            {item.items.map((page) => (
+          </button>
+          <div
+            className={`${
+              dropdown ? "block" : "hidden"
+            } lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}
+          >
+            <ul className="bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden">
+              {item.items.map((page) => (
                 <a
                   href="/"
                   className="hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft"
@@ -42,7 +57,7 @@ const NavItem = ({ item }) => {
               ))}
             </ul>
           </div>
-        </>
+        </div>
       )}
     </li>
   );
@@ -58,7 +73,7 @@ const Header = () => {
   };
 
   return (
-    <section>
+    <section className="sticky top-0 left-0 right-0 z-50">
       <header className="container mx-auto px-5 flex justify-between py-4 items-center">
         <div>
           <img className="w-16" src={images.Logo} alt="logo" />
@@ -74,8 +89,9 @@ const Header = () => {
           )}
         </div>
         <div
-          className={`${navIsVisible ? "right-0" : "-right-full"
-            } transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center`}
+          className={`${
+            navIsVisible ? "right-0" : "-right-full"
+          } transition-all duration-300 mt-[56px] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto justify-center lg:justify-end lg:flex-row fixed top-0 bottom-0 lg:static gap-x-9 items-center`}
         >
           <ul className="text-white items-center gap-y-5 lg:text-dark-soft flex flex-col lg:flex-row gap-x-2 font-semibold">
             {navItemsInfo.map((item) => (
